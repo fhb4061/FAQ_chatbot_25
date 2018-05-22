46bf5820af9082005400e1e0c97b64fbf623f6c1
Jonathan <fhb4061@autuni.ac.nz>
2018-05-22 18:51:55 +1200

    Changing a few things on server.js file

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-// installing in the server
+
 const express = require('express');
+const app = express();
+
 const path = require('path');
 
-const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
-
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/index.html'));
