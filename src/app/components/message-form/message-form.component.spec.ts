3300bbc630adfa3b13c84c34c9af5f0c75375d40
Eric Lam <pkz5183@autuni.ac.nz>
2018-05-22 13:38:46 +1200

    updated unit test

--- src/app/components/message-form/message-form.component.spec.ts
+++ src/app/components/message-form/message-form.component.spec.ts
@@ -3,12 +3,13 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MessageFormComponent } from './message-form.component';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Message } from '../../models/message';
 import { DialogflowService } from '../../services/dialogflow.service';
 
 describe('MessageFormComponent', () => {
   let component: MessageFormComponent;
   let fixture: ComponentFixture<MessageFormComponent>;
-
+  let testdialoglow : DialogflowService;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ MessageFormComponent ],
@@ -22,8 +23,18 @@ describe('MessageFormComponent', () => {
     });
   }));
 
+  //Test if 2 string are equal
+  let expectedmessage = new Message('Hi there, may I get your name please?', '', new Date());
+  let testmessagecontent = 'Hi there, may I get your name please?';
 
+  it('expected message equals as the tested message', () => {
+    expect(testmessagecontent).toEqual(expectedmessage.content);
+  });
+
+//Test if the component is created
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
 });
