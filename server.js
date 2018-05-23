37851fc2dbb189b6787751a055d9ca9f093ddb8f
Eric Lam <pkz5183@autuni.ac.nz>
2018-05-23 23:51:54 +1200

    new update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/chatbot25/index.html'));
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.set('port', port);
 
 const server = http.createServer(app);
