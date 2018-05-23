38270ca606c69e2ad28a09634d379a675e88fce0
Eric Lam <pkz5183@autuni.ac.nz>
2018-05-23 23:02:28 +1200

    update now

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const app = express();
+const http = require('http')
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
-app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
+const app = express();
 
-console.log('Console Listensing!');
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
