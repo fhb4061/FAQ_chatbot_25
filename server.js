29044969585ae79f264b04eb037d1bda8c97e22b
Jonathan <fhb4061@autuni.ac.nz>
2018-05-23 14:46:18 +1200

    still changing the path

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const app = express();
 const path = require('path');
 
 
-// Serve only the static files form the dist directory
+// Serve only the static files from the dist directory
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
 //PathLocationStrategy
 app.get('/*', function(req,res) {    
-    res.sendFile(path.join(__dirname+'../chatbot25/dist/chatbot25/index.html'));
+    res.sendFile(path.join(__dirname+'chatbot25/dist/chatbot25/index.html'));
     });
