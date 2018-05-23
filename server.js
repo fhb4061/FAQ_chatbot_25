d82bc21403b5a2e3a717ce46505d4c75666c8b92
Jonathan <fhb4061@autuni.ac.nz>
2018-05-23 16:12:30 +1200

    Trying something different as 404 error comes up. - Trying creating a procfile

--- server.js
+++ server.js
@@ -2,19 +2,6 @@
 const express = require('express');
 const app = express();
 
-// if protocol is used are NOT HTTPS
-// redirect it to the same url but with HTTPS
-const forceSSL = function(){
-    return function (req, res, next){
-        if (res.headers['x-forward-proto'] !== 'https')
-        {
-            return res.redirect(
-                ['https://', req.get('Host'), req.url].join('')
-            );
-        }
-        next();
-    }
-}
 // Serve only the static files from the dist directory
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default Heroku port
